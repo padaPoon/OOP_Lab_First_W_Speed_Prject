1e1c4e4e0cdd09d724ee3d7c7a28e21685967b5d
padaPoon <padawankerdphol@gmail.com>
2025-11-24 16:18:11 +0700

    groundCheck

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,20 +1,29 @@
 using UnityEngine;
+using UnityEngine.Accessibility;
 
 public class CharacterMovement : MonoBehaviour
 {
+    public float maxSpeed;
     public float moveSpeed = 5f;
     public float jumpForce = 7f;
 
+    public bool grounded = false;
+    public Transform groundCheck;
+    float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+    
     [field: SerializeField]public int Coin {  get;  set; } = 0;
     [field: SerializeField]public int Stamina { get; set; } = 10;
 
 
 
     private Rigidbody2D rb;
-
+    Animator anim;
+    
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
@@ -43,9 +52,19 @@ public class CharacterMovement : MonoBehaviour
     }
 
 
+    public void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        anim.SetBool("Ground", grounded);
 
+        anim.SetFloat("vSpeed", rb.linearVelocity.y);
 
+        float move = Input.GetAxis("Horizontal");
 
+        anim.SetFloat("Speed", Mathf.Abs(move));
+
+        rb.linearVelocity = new Vector2(move * maxSpeed, rb.linearVelocity.y);
+    }
 
     public void OnTriggerEnter2D(Collider2D other)
     {
